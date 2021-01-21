00923d3a09ffe088c64eaf0a8c4c7b0448b55162
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2021-01-22 00:34:29 +0300

    Добавлено async await и стили

--- src/index.js
+++ src/index.js
@@ -1,31 +1,58 @@
 import './styles.css';
 import featchImage from './js/apiService.js';
-import cardTemplate from './templates/card-template.hbs';
+// import cardTemplate from './templates/card-template.hbs';
+import cardTemplate from './templates/card-template-bootstrap.hbs';
+import messageTemplate from './templates/message-template.hbs';
 
 const input = document.querySelector('input');
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const button = document.querySelector('button');
+const div = document.querySelector('.row');
+const toastContainer = document.querySelector('.toast-container');
 
 let page = 1;
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  gallery.innerHTML = '';
-  featchImage(input.value, page).then(data => formHandler(data));
+  // gallery.innerHTML = '';
+  div.innerHTML = '';
+  page = 1;
+  fetchResult();
+  button.disabled = false;
 });
 
 button.addEventListener('click', buttonHandler);
 
+async function fetchResult() {
+  // featchImage(input.value, page).then(data => formHandler(data));
+  try {
+    await featchImage(input.value, page)
+      .then(({ hits }) => hits)
+      .then(data => formHandler(data));
+  } catch (error) {
+    toastContainer.insertAdjacentHTML('afterbegin', messageTemplate(error));
+    document.querySelector('.btn-close').addEventListener('click', () => {
+      toastContainer.innerHTML = '';
+    });
+    setTimeout(() => {
+      toastContainer.innerHTML = '';
+      btnClose.removeEventListener();
+    }, 10000);
+  }
+}
+
 function formHandler(element) {
-  gallery.insertAdjacentHTML('beforeend', cardTemplate(element));
+  // gallery.insertAdjacentHTML('beforeend', cardTemplate(element));
+  div.insertAdjacentHTML('beforeend', cardTemplate(element));
+
+  window.scrollTo({
+    top: document.documentElement.offsetHeight,
+    behavior: 'smooth',
+  });
 }
 
 function buttonHandler() {
   page += 1;
-  featchImage(input.value, page).then(data => formHandler(data));
-  window.scrollTo({
-    top: 1000,
-    behavior: 'smooth',
-  });
+  fetchResult();
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,10 +2,19 @@
 
 const apiKey = '19902573-b9fa82d62327bd625e4b4b636';
 
-function featchImage(searchQuery, page) {
+async function featchImage(searchQuery, page) {
   const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${apiKey}`;
-  return fetch(url)
-    .then(res => res.json())
-    .then(({ hits }) => hits);
+  //  fetch(url)
+  //   .then(res => res.json())
+  //   .then(({ hits }) => hits);
+  try {
+    const response = await fetch(url);
+    const images = response.json();
+    // throw 'oops';
+    return images;
+  } catch (error) {
+    throw error;
+  }
 }
+
 export default featchImage;
