d295d68bf82c45b3b8416fd4ebce482a8a0d5dcb
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2021-01-23 23:57:17 +0300

    Экспорт объекта, обработка ошибок

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
-import featchImage from './js/apiService.js';
+// import featchImage from './js/apiService.js';
+import apiService from './js/apiService.js';
 // import cardTemplate from './templates/card-template.hbs';
 import cardTemplate from './templates/card-template-bootstrap.hbs';
 import messageTemplate from './templates/message-template.hbs';
@@ -11,35 +12,50 @@ const button = document.querySelector('button');
 const div = document.querySelector('.row');
 const toastContainer = document.querySelector('.toast-container');
 
-let page = 1;
+// let page = 1;
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   // gallery.innerHTML = '';
   div.innerHTML = '';
-  page = 1;
+  // page = 1;
+  apiService.resetPage();
   fetchResult();
   button.disabled = false;
 });
 
 button.addEventListener('click', buttonHandler);
 
-async function fetchResult() {
+// async function fetchResult() {
+//   // featchImage(input.value, page).then(data => formHandler(data));
+//   try {
+//     await featchImage(input.value, page)
+//       .then(({ hits }) => hits)
+//       .then(data => {
+//         if (data.length > 0) {
+//           formHandler(data);
+//         } else {
+//           errorHandler('Совпадений нет');
+//         }
+//       });
+//   } catch (error) {
+//     errorHandler(error);
+//   }
+// }
+
+function fetchResult() {
   // featchImage(input.value, page).then(data => formHandler(data));
-  try {
-    await featchImage(input.value, page)
-      .then(({ hits }) => hits)
-      .then(data => formHandler(data));
-  } catch (error) {
-    toastContainer.insertAdjacentHTML('afterbegin', messageTemplate(error));
-    document.querySelector('.btn-close').addEventListener('click', () => {
-      toastContainer.innerHTML = '';
+  apiService.query = input.value;
+  apiService
+    .fetchImage()
+    // .then(({ hits }) => hits)
+    .then(data => {
+      if (data.length > 0) {
+        formHandler(data);
+      } else {
+        errorHandler('Совпадений нет');
+      }
     });
-    setTimeout(() => {
-      toastContainer.innerHTML = '';
-      btnClose.removeEventListener();
-    }, 10000);
-  }
 }
 
 function formHandler(element) {
@@ -53,6 +69,13 @@ function formHandler(element) {
 }
 
 function buttonHandler() {
-  page += 1;
+  // page += 1;
   fetchResult();
+}
+
+function errorHandler(error) {
+  toastContainer.insertAdjacentHTML('afterbegin', messageTemplate(error));
+  document.querySelector('.btn-close').addEventListener('click', () => {
+    toastContainer.innerHTML = '';
+  });
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,19 +2,50 @@
 
 const apiKey = '19902573-b9fa82d62327bd625e4b4b636';
 
-async function featchImage(searchQuery, page) {
-  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${apiKey}`;
-  //  fetch(url)
-  //   .then(res => res.json())
-  //   .then(({ hits }) => hits);
-  try {
-    const response = await fetch(url);
-    const images = response.json();
-    // throw 'oops';
-    return images;
-  } catch (error) {
-    throw error;
-  }
-}
+// async function featchImage(searchQuery, page) {
+//   const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${apiKey}`;
+//   //  fetch(url)
+//   //   .then(res => res.json())
+//   //   .then(({ hits }) => hits);
+//   try {
+//     const response = await fetch(url);
+//     const images = response.json();
+//     // throw 'oops';
+//     return images;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
-export default featchImage;
+// export default featchImage;
+
+export default {
+  searchQuery: '',
+  page: 1,
+
+  fetchImage() {
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${apiKey}`;
+    return fetch(url)
+      .then(res => res.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  },
+
+  incrementPage() {
+    this.page += 1;
+  },
+
+  resetPage() {
+    this.page = 1;
+  },
+
+  get query() {
+    return this.searchQuery;
+  },
+
+  set query(value) {
+    this.searchQuery = value;
+  },
+};
